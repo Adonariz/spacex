dd67eca0f5acf815623ff5ed755ea5d256c2eaf9
A Zhila <aleksej.zhila@gmail.com>
2020-10-16 00:14:17 +0300

    adds fetch data via api and implements it in components

--- src/App.js
+++ src/App.js
@@ -5,17 +5,48 @@ import Features from "./Components/Features/Features";
 import Footer from "./Components/Footer/Footer";
 import Calendar from "./Components/Calendar/Calendar";
 import Details from "./Components/Details/Details";
+import FetchData from "./service/FetchData";
 import './style.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header />
-      <Main />
-      <Features />
-      <Footer />
-    </React.Fragment>
-  );
+class App extends React.Component {
+  fetchData = new FetchData();
+
+  state = {
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: [],
+  }
+
+  componentDidMount() {
+    this.updateRocket();
+  }
+
+  updateRocket() {
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({ rockets: data.map(item => item.name)});
+
+        return data;
+      }).then(data => data.find(item => item.name === this.state.rocket))
+      .then(rocketFeatures => this.setState({ rocketFeatures }));
+  }
+
+  changeRocket = rocket => {
+    this.setState({
+      rocket
+    }, this.updateRocket);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket}/>
+        <Main rocket={this.state.rocket}/>
+        <Features />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from "../../logo.svg";
 import './header.css';
 
-const Header = () => (
+const Header = (props) => (
   <header className="header">
     <img
       src={logo}
@@ -11,24 +11,30 @@ const Header = () => (
     />
     <nav className="main-nav nav">
       <ul className="list">
+        {props.rockets.map((item) => (
+          <li key={item} className="item">
+            <a
+              href="/"
+              onClick={evt => {
+                evt.preventDefault();
+                props.changeRocket(item);
+              }}
+              className="item-link"
+            >
+              {item}
+            </a>
+          </li>
+        ))}
+
         <li className="item">
-          <a href="#" className="item-link">Falcon 1</a>
-        </li>
-        <li className="item">
-          <a href="#" className="item-link">Falcon 9</a>
-        </li>
-        <li className="item">
-          <a href="#" className="item-link">Falcon Heavy</a>
-        </li>
-        <li className="item">
-          <a href="#" className="item-link">Updates</a>
+          <a href="/" className="item-link">Updates</a>
         </li>
       </ul>
     </nav>
     <nav className="secondary-nav">
       <ul className="list">
         <li className="item">
-          <a href="#" className="item-link">Home</a>
+          <a href="/" className="item-link">Home</a>
         </li>
         <li className="item">
           <a href="calendar.html" className="item-link">Calendar</a>
